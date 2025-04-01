6996099496b5719b6858f2cb7dae5022b574f090
umutdiyar <umutdiyarbalci@gmail.com>
2025-04-01 23:21:40 +0300

    hotfix

--- src/components/sideNavbar.vue
+++ src/components/sideNavbar.vue
@@ -1,24 +1,28 @@
 <template>
-  <header class="sticky top-0 bg-weather-primary shadow-lg py-4">
-    <nav class="container flex mx-auto items-center justify-between max-w-2xl">
-      <RouterLink :to="{ name: 'home' }" class="flex items-center space-x-2">
-        <i class="fa-solid fa-sun text-white text-2xl"></i>
-        <p class="text-white text-2xl">The Local Weather</p>
-      </RouterLink>
-      <div class="flex gap-3 items-end justify-end mr-0">
-        <i
-          class="fa-solid fa-circle-info text-white text-xl hover:text-gray-300 duration-150 cursor-pointer"
-        ></i>
-        <i
-          class="fa-solid fa-plus text-white text-xl hover:text-gray-300 duration-150 cursor-pointer"
-        ></i>
+  <header class="fixed top-0 left-0 right-0 bg-weather-primary shadow-2xl">
+    <nav class="flex justify-center items-center text-white py-6">
+      <div class="flex justify-between items-center w-full max-w-screen-lg">
+        <RouterLink :to="{ name: 'home' }">
+          <div class="flex items-center space-x-4">
+            <i class="fa-solid fa-sun text-2xl"></i>
+            <p class="text-2xl">The Local Weather</p>
+          </div>
+        </RouterLink>
+
+        <div class="flex items-center text-white space-x-4 text-xl">
+          <i
+            class="fa-solid fa-circle-info hover:text-weather-secondary duration-150 cursor-pointer"
+          ></i>
+          <i class="fa-solid fa-plus hover:text-weather-secondary duration-150 cursor-pointer"></i>
+        </div>
       </div>
     </nav>
   </header>
 </template>
 
-<script>
+<script setup>
 import { RouterLink } from 'vue-router'
+import BaseModal from './baseModal.vue'
 </script>
 
 <style lang="scss" scoped></style>
